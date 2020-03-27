1d16df49bf9bb769e0f3bd126c28b7d492dc4c03
gpi <gpimpale29@gmail.com>
2020-03-27 01:07:39 -0700

    parser improved

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,6 @@
 pub mod tokenizer;
 pub mod parser;
 
-// Definitions from here:
-// https://web.stanford.edu/~jurafsky/slp3/8.pdf
-pub enum PartOfSpeech {
-    CC,      // coordinating conjunction
-    CD,      // cardinal number
-    DT,      // determiner
-    EX,      // existential there
-    FW,      // foreign word
-    IN,      // preposition/subordinating conjunction
-    JJ,      // adjective
-    JJR,     // comparative adjective
-    JJS,     // superlative adjective
-    LS,      // list item marker
-    MD,      // modal
-    NN,      // singular or mass noun
-    NNS,     // noun plural
-    NNP,     // proper noun, singular
-    NNPS,    // proper noun, plural
-    PDT,     // predeterminer
-    POS,     // possessive ending
-    PRP,     // personal pronoun
-    PRP_S,   // possessive pronoun
-    RB,      // adverb
-    RBR,     // comparative adverb
-    RBS,     // superlative adverb
-    RP,      // particle
-    SYM,     // symbole
-    TO,      // to
-    UH,      // interjection
-    VB,      // verb base form
-    VBD,     // verb past tense
-    VBG,     // verb gerund
-    VBN,     // verb past participle
-    VBP,     // verb non 3sg persent
-    VBZ,     // verb 3sg present
-    WDT,     // wh determine
-    WP,      // wh pronoun
-    WP_S,    // wh posess
-    WRB,     // wh adverb
-    DOLLAR,  // $
-    HASH,    // #
-    LQUOTE,  // "
-    RQUOTE,  // "
-    LPAREN,  // (
-    RPAREN,  // )
-    COMMA,   // ,
-    ENDPUNC, // .
-    MIDPUNC, // ;
-}
 
 // 'doc is the lifetime of the doc
 // 'vocab is the lifetime of the vocab
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,58 @@
 use super::Token;
 use std::collections::HashSet;
 use std::collections::VecDeque;
-use std::iter::FromIterator;
+use std::iter;
+
+// Definitions from here:
+// https://web.stanford.edu/~jurafsky/slp3/8.pdf
+#[derive(std::hash::Hash, Clone, Copy, std::cmp::Eq, std::cmp::PartialEq)]
+pub enum PartOfSpeech {
+    CC,      // coordinating conjunction
+    CD,      // cardinal number
+    DT,      // determiner
+    EX,      // existential there
+    FW,      // foreign word
+    IN,      // preposition/subordinating conjunction
+    JJ,      // adjective
+    JJR,     // comparative adjective
+    JJS,     // superlative adjective
+    LS,      // list item marker
+    MD,      // modal
+    NN,      // singular or mass noun
+    NNS,     // noun plural
+    NNP,     // proper noun, singular
+    NNPS,    // proper noun, plural
+    PDT,     // predeterminer
+    POS,     // possessive ending
+    PRP,     // personal pronoun
+    PRP_S,   // possessive pronoun
+    RB,      // adverb
+    RBR,     // comparative adverb
+    RBS,     // superlative adverb
+    RP,      // particle
+    SYM,     // symbole
+    TO,      // to
+    UH,      // interjection
+    VB,      // verb base form
+    VBD,     // verb past tense
+    VBG,     // verb gerund
+    VBN,     // verb past participle
+    VBP,     // verb non 3sg persent
+    VBZ,     // verb 3sg present
+    WDT,     // wh determine
+    WP,      // wh pronoun
+    WP_S,    // wh posess
+    WRB,     // wh adverb
+    DOLLAR,  // $
+    HASH,    // #
+    LQUOTE,  // "
+    RQUOTE,  // "
+    LPAREN,  // (
+    RPAREN,  // )
+    COMMA,   // ,
+    ENDPUNC, // .
+    MIDPUNC, // ;
+}
 
 pub enum Moves {
     Shift,
@@ -9,53 +60,71 @@ pub enum Moves {
     RightArc,
 }
 
-
-#[derive(std::hash::Hash)]
-#[derive(std::cmp::Eq)]
-#[derive(std::cmp::PartialEq)]
+#[derive(std::hash::Hash, std::cmp::Eq, std::cmp::PartialEq)]
 struct TokenArc {
-    parent: Option<TokenIndex>,
-    child:TokenIndex,
+    parent: TokenIndex,
+    child: TokenIndex,
+    label: PartOfSpeech
 }
 
-#[derive(std::hash::Hash)]
-#[derive(std::cmp::Eq)]
-#[derive(std::cmp::PartialEq)]
-pub struct TokenIndex {
-    value:usize,
+#[derive(std::hash::Hash, Clone, Copy, std::cmp::Eq, std::cmp::PartialEq)]
+pub enum TokenIndex {
+    Root,
+    Value(usize),
 }
+
 
 pub struct Parser<'doc, 'vocab> {
-    token_backing:Vec<Token<'doc, 'vocab>>,
-    queue:VecDeque<TokenIndex>,
-    stack:Vec<TokenIndex>,
-    arcs:HashSet<TokenArc>,
+    token_backing: Vec<Token<'doc, 'vocab>>,
+    queue: VecDeque<TokenIndex>,
+    stack: Vec<TokenIndex>,
+    arcs: HashSet<TokenArc>,
 }
 
 impl<'doc, 'vocab> Parser<'doc, 'vocab> {
-
     // Create new parse tree
-    pub fn new(sentence:Vec<Token<'doc, 'vocab>>) -> Parser<'doc, 'vocab> {
+    pub fn new(sentence: Vec<Token<'doc, 'vocab>>) -> Parser<'doc, 'vocab> {
         Parser {
-            queue: (0..sentence.len()).map(|value| TokenIndex {value}).collect(),
+            queue: (0..sentence.len())
+                .map(|value| TokenIndex::Value(value))
+                .collect(),
             token_backing: sentence,
-            stack: Vec::new(),
-            arcs: HashSet::new()
+            stack: vec![TokenIndex::Root],
+            arcs: HashSet::new(),
         }
     }
 
-    fn queue_empty(&self) -> bool {
+    pub fn queue_empty(&self) -> bool {
         self.queue.len() == 0
     }
 
-    // Make sure queue is not empty
-    fn shift(&mut self) -> () {
+    pub fn stack_arcable(&self) -> bool {
+        self.stack.len() > 1
+    }
+
+    // pseudocode taken from: https://www.diva-portal.org/smash/get/diva2:661423/FULLTEXT01.pdf
+
+    // Dequeues the topmost node onto the stack
+    // Make sure queue is not empty before using
+    pub fn shift(&mut self) -> () {
         self.stack.push(self.queue.pop_front().unwrap());
     }
 
+    // adds new arc with label from the topmost node on the stack to second topmost node on the
+    // stack, then removes the second topmost node
     // Ensure there are 2 nodes on the stack
-    pub fn left_arc(&mut self, label:&str) {
-        
+    pub fn left_arc(&mut self, label: PartOfSpeech) {
+        let parent = *self.stack.last().unwrap();
+        let child = self.stack.remove(self.stack.len() - 2);
+        self.arcs.insert(TokenArc { parent, child, label});
     }
 
+    // adds new arc with label from the second topmost node on the stack to the topmost node and
+    // removes the topmost node
+    // Ensure there are 2 nodes on the stack
+    pub fn right_arc(&mut self, label: PartOfSpeech) {
+        let parent = *self.stack.get(self.stack.len() - 2).unwrap();
+        let child = self.stack.pop().unwrap();
+        self.arcs.insert(TokenArc { parent, child, label});
+    }
 }
